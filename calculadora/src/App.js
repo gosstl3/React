4c0ace6aa4f91f1c71adbd9b8e4897eef227981c
Eugenio Juárez <eugeniogly@gmail.com>
2022-06-08 18:18:01 -0500

    Estructura
    Se crearon los componentes de los botones, asignación de estilos, una función para saber si es un numero o un operador y estilos para los botones.

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,9 +1,45 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/FreeCodeCamp_logo.png';
+import Boton from './componentes/Boton';
 
 function App() {
   return (
     <div className="App">
-     Hola
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={ freeCodeCampLogo }
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp' />
+
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='fila'></div>
+      </div>
+     
     </div>
   );
 }
