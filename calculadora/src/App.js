97f16eb6e0d378d49ed2d619a6673c4b8ccf21f9
Eugenio Juárez <eugeniogly@gmail.com>
2022-06-15 17:59:00 -0500

    Terminando calculadora basica
    Se agrego el metodo para hacer las operaciones, se agrega la clase mathjs mediante npm y se corrigieron los warnings por el simbolo de comparacion.

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -4,6 +4,7 @@ import Boton from './componentes/Boton';
 import Pantalla from './componentes/Pantalla';
 import BotonClear from './componentes/BotonClear';
 import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
 
@@ -11,6 +12,14 @@ function App() {
 
   const agregarInput = val =>{
     setInput(input + val);
+  };
+
+  const calucularResultado = () =>{
+    if  (input){
+    setInput(evaluate(input));
+    }else{
+      alert("Por favor ingresa un valor");
+    }
   };
 
   return (
@@ -43,7 +52,7 @@ function App() {
           <Boton manejarClic={agregarInput}>+</Boton>
         </div>
         <div className='fila'>
-          <Boton manejarClic={agregarInput}>=</Boton>
+          <Boton manejarClic={calucularResultado}>=</Boton>
           <Boton manejarClic={agregarInput}>0</Boton>
           <Boton manejarClic={agregarInput}>.</Boton>
           <Boton manejarClic={agregarInput}>/</Boton>
