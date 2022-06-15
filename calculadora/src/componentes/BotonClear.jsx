550e2594df191ab5ad7176a4588b7e7b6bc3d4d4
Eugenio Juárez <eugeniogly@gmail.com>
2022-06-15 15:22:41 -0500

    Boton clear
    Se definieron dos tipos de funciones y se maneja el evento onclick para el funcionamiento de la funcion manejarclear

--- calculadora/src/componentes/BotonClear.jsx
+++ calculadora/src/componentes/BotonClear.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../hojas-de-estilo/Clear.css';
 
 const BotonClear = (props) => (
-    <div className='boton-clear'>
+    <div className='boton-clear' onClick={props.manejarClear}>
       {props.children}
     </div>
 );
